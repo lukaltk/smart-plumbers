896d783ef6191e019a78415922f316b83dcbd6ef
Luka Takeshi Kato <luka.kato@tuntscorp.com>
2021-12-11 07:01:07 -0300

    feat: mocked data

--- mixins/csv.mixin.js
+++ mixins/csv.mixin.js
@@ -3,6 +3,7 @@
 const fs = require('fs');
 const csvParser = require('csv').parse;
 const createObjectCsvWriter = require('csv-writer').createObjectCsvWriter;
+const { MoleculerError } = require('moleculer').Errors;
 
 module.exports = {
   name: 'csv-mixin',
@@ -20,62 +21,72 @@ module.exports = {
 
     async saveCsv(data) {
 
-      const dir = `./csv/${data.id}`;
+      try {
+        const dir = `./csv/${data.id}`;
 
-      if(!fs.existsSync(dir)) fs.mkdirSync(dir);
+        if(!fs.existsSync(dir)) fs.mkdirSync(dir);
+  
+        const csvWriter = createObjectCsvWriter({
+          path: `${dir}/${data.timestamp.split('T')[0]}.csv`,
+          header: [
+            { id: 'id', title: 'Id'},
+            { id: 'temperature', title: 'Temperature'},
+            { id: 'timestamp', title: 'Timestamp'},
+          ],
+          append: true
+        });
+  
+        await csvWriter.writeRecords([data]);
 
-      const csvWriter = createObjectCsvWriter({
-        path: `${dir}/${data.timestamp.split('T')[0]}.csv`,
-        header: [
-          { id: 'id', title: 'Id'},
-          { id: 'temperature', title: 'Temperature'},
-          { id: 'timestamp', title: 'Timestamp'},
-        ],
-        append: true
-      });
-
-      await csvWriter.writeRecords([data]);
-    
+      } catch(error) {
+        throw new MoleculerError('Error to save CSV', 500, 'ERR_SAVE_CSV', error);
+      }    
     },
 
     async readCsv(id, date) {
 
-      const dir = `./csv/${id}/${date}.csv`;
+      try {
+        const dir = `./csv/${id}/${date}.csv`;
 
-      const data = [];
-      
-      await new Promise((resolve, reject) => {
-        fs.createReadStream(dir).pipe(csvParser())
-          .on('data', (row) => {
-            data.push({
-              id: row[0],
-              temperature: row[1],
-              timestamp: row[2],
+        const data = [];
+        
+        await new Promise((resolve, reject) => {
+          fs.createReadStream(dir).pipe(csvParser())
+            .on('data', (row) => {
+              data.push({
+                id: row[0],
+                temperature: row[1],
+                timestamp: row[2],
+              });
+            })
+            .on('end', () => {
+              console.log('CSV file successfully processed');
+              resolve();
             });
-          })
-          .on('end', () => {
-            console.log('CSV file successfully processed');
-            resolve();
-          });
-      });
+        });
 
-      return data;
-
+        return data;
+      } catch (error) {
+        throw new MoleculerError('Error to read CSV', 500, 'ERR_READ_CSV', error);
+      }
     },
 
     async readLastTemperatureCsv(id) {
 
-      const folder = `./csv/${id}`
+      try {
+        const folder = `./csv/${id}`
       
-      const files = fs.readdirSync(folder);
-      const file = files.sort().reverse()[0];
-      const date = file.split('.')[0]
-
-      const temperature = await this.readCsv(id, date);
-
-      return  temperature[temperature.length - 1];
+        const files = fs.readdirSync(folder);
+        const file = files.sort().reverse()[0];
+        const date = file.split('.')[0]
+  
+        const temperature = await this.readCsv(id, date);
+  
+        return  temperature[temperature.length - 1];
+      } catch (error) {
+        throw new MoleculerError('Error to read last temperature of CSV', 500, 'ERR_READ_LAST_TEMPERATURE_CSV', error);
+      }
     }
-
   },
 
   /**
